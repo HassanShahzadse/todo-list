8ef16685cf20bee89d58e642ee8d7f3500801f2b
hassanshahzadse <hassanshahzadvs@gmail.com>
2023-02-04 20:35:39 +0500

    added funtonality to read user input

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TaskInput from "./component/Taskinput/Taskinput"
+import TodoTask from './component/todotask/Todotask';
+function App() {
+  return (
+    <div>
+      <TaskInput />
+      
+    </div>
+  );
+}
+
+export default App;
